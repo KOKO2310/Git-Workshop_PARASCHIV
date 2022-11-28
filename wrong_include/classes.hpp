7c7346bf5f2855b4c367e8b20b32d098580980c3
KOKO2310 <95043488+KOKO2310@users.noreply.github.com>
2022-11-28 09:35:10 +0200

    Add files via upload
    faza 2.2 vama veche pe viata

--- wrong_include/classes.hpp
+++ wrong_include/classes.hpp
@@ -1,6 +1,86 @@
 #include "libs.hpp"
 
-// TODO move Student here
+class Student
+{
+    private:
+        int id;
+        std::string name;
+        std::vector<double> grades;
+        double average;
+
+    public:
+        Student(int id, std::string name, std::vector<double> grades);
+
+        int getId();
+        std::string getName();
+        std::vector<double> getGrades();
+        double getAverage();
+        
+        void calculateAverage()
+        {
+        	double s = 0;
+        	int i, n = grades.size();
+        	
+        	for(i = 0; i < n; i++)
+        	{
+        		cout<<grades[i]<<' ';
+        		s = s + grades[i];
+			}
+			
+			average = s/n;
+			
+			cout<<endl<<average<<endl;
+	}
+        bool hasFailed();
+
+        friend std::ostream& operator<<(std::ostream& out, const Student& student);
+};
+
+Student::Student(int id, std::string name, std::vector<double> grades)
+{
+    this->id = id;
+    this->name = name;
+    this->grades = grades;
+}
+
+int Student::getId()
+{
+    return this->id;
+}
+
+std::string Student::getName()
+{
+    return this->name;
+}
+
+std::vector<double> Student::getGrades()
+{
+    return this->grades;
+}
+
+double Student::getAverage()
+{
+    return this->average;
+}
+
+bool Student::hasFailed()
+{
+    return this->average > 5.0;
+}
+
+std::ostream& operator<<(std::ostream& out, const Student& student)
+{
+    out << "ID: " << student.id << "\n" << "Name: " << student.name << "\n" << "Grades: [ ";
+
+    for(double i : student.grades)
+    {
+        out << i << " ";
+    }
+
+    out << "]\n" << "Average: " << student.average << "\n";
+
+    return out;
+}
 
 class University
 {
